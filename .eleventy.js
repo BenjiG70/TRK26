e8fe71f7a031a525a9fb5ce148a9b8322e199133
Benji <markus.tabler@tutanota.com>
2026-02-07 21:48:25 +0100

    Configure passthrough copy and path prefix
    Added passthrough copy for CSS assets and updated directory structure with a path prefix.

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,15 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('src/images')
+  eleventyConfig.addPassthroughCopy('src/images');
+  eleventyConfig.addPassthroughCopy('src/assets/css');
 
   return {
-    dir: { input: 'src', output: 'dist', data: '_data' },
-    passthroughFileCopy: true,
+    pathPrefix: "/TRK26/",
+    dir: { 
+      input: 'src', 
+      output: 'dist', 
+      data: '_data' 
+    },
     templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
     htmlTemplateEngine: 'njk'
-  }
-}
+  };
+};
