23f751d4a606507ed6445e319e6621e53dff0fb8
BenjiG70 <markus.tabler@tutanota.com>
2026-02-07 22:08:10 +0100

    Upgrade: Eleventy updated to v3.0

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/assets/css');
 
+  eleventyConfig.addWatchTarget("src/assets/css/");
+
   return {
     pathPrefix: "/TRK26/",
     dir: { 
@@ -9,7 +11,7 @@ module.exports = function(eleventyConfig) {
       output: 'dist', 
       data: '_data' 
     },
-    templateFormats: ['njk', 'md', 'css', 'html', 'yml'],
+    templateFormats: ['njk', 'md', 'html', 'yml'],
     htmlTemplateEngine: 'njk'
   };
 };
